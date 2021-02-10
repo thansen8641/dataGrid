d658df8488512f5ac4ab888308fd7451afe81fe5
Timothy Hansen <timothyhansen229@gmail.com>
2021-02-09 18:11:44 -0800

    Very basic home page up

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Home from './Home.jsx';
 
 
 const App = () => {
   return (
     <div>
-      hello from app
+      <Home />
     </div>
   )
 }
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Home = () => {
+  return (
+    <div>
+      <div className='homeContainer'>
+        <div className='homeText'>
+          <h3> Timothy Hansen </h3>
+          <h6> Software Engineer </h6>
+          <p> JAVASCRIPT | REACT | HTML | CSS | NODE | EXPRESS | MONGODB | MYSQL | POSTGRESQL | AWS  </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;

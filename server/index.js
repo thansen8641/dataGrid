3f90791d708cd4edbe1fa0f37579f5418d420a2c
Timothy Hansen <timothyhansen229@gmail.com>
2021-02-13 16:04:20 -0800

    make it better

--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
+// require('dotenv').config()
 const express = require('express')
 const app = express();
-// const port = 3000;
-require('dotenv').config()
+const port = process.env.PORT || 3000
 const path = require('path');
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
-// app.get('/', function (req, res) {
-//   res.send('hello world')
-// })
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(() => {
-  console.log(`Portfolio listening at http://localhost:${process.env.PORT}`)
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: path.join(__dirname, '../client/dist/') });
+});
+// app.get('/About', function (req, res) {
+//   res.sendFile('index.html', { root: path.join(__dirname, '../client/dist/') });
+// });
+
+// app.get('/Projects', function (req, res) {
+//   res.sendFile('index.html', { root: path.join(__dirname, '../client/dist/') });
+// });
+// app.get('/Contact', function (req, res) {
+//   res.sendFile('index.html', { root: path.join(__dirname, '../client/dist/') });
+// });
+app.use(bodyParser.json())
+
+
+app.listen(port, () => {
+  console.log(`Portfolio listening at http://localhost:${port}`)
 })

53d4842d1d9e19fc3875c2ed870752a6c53cabc3
Timothy Hansen <timothyhansen229@gmail.com>
2021-02-15 13:33:02 -0800

    make it better

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -14,9 +14,9 @@ const About = (props) => {
         <div className='selfieContainer'>
           <img className='selfie' src={props.darkMode ? 'https://i.ibb.co/1ZjVzTP/pic.png' : 'https://i.ibb.co/ScLDcrd/BandW.png'} />
           <div className='aboutInfo'>
-            {/* <p >
+            <p >
               The feeling I get when I produce clean and efficient code that works just as I intended parallels the moment in Cast Away when Tom Hanks finally creates fire on his deserted island. It has become an unbelievably fulfilling aspect of my life and keeps me coming back for more. To replicate that same feeling of excitement in a user utilizing an application I worked on is one of the reasons I started programming in the first place.
-      </p> */}
+      </p>
             <p>
               The future of society as we know it is technology. I want to be a part of shaping that future and influence it in a positive manner while simultaneously working on projects that I’m passionate about. It is my dream to build applications for the betterment of the world and to course correct some of the negative light big technology has been receiving lately. Life can be extremely hard at times, so I want to build applications that lift the burdens of life, even if only in a small way.
       </p>
